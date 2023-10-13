e27429bd9ac0840cfceb8a2c86f4977e2e7f1766
unknown <patrakeev.matvej@bk.ru>
2023-10-13 16:59:38 +0300

    add basic layout and react-func

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
+function App() {
+
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <NotFound />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Categories from "../components/Categories";
+import Sort from "../components/Sort";
+import Skeleton from "../components/Pizza/Skeleton";
+import Pizza from "../components/Pizza";
+
+export default function Home() {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    fetch("https://6527cdaf931d71583df169fa.mockapi.io/items")
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
+  return (
+    <>
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(8)].map((_, index) => <Skeleton key={index} />)
+          : items.map((obj) => <Pizza key={obj.id} {...obj} />)}
+      </div>
+    </>
+  );
+}
